320dad672be0711c29b8c84e5516dc450c958fb5
Heather Booker <heather.booker@hotmail.com>
2017-04-18 00:49:48 -0700

    Add ability to extract threads!

--- perceval-mbox.py
+++ perceval-mbox.py
@@ -1,6 +1,7 @@
 from perceval.backends.core.mbox import MBox
 from elasticsearch import Elasticsearch
 import hashlib
+from jwzthreading import thread, Message
 es = Elasticsearch()
 
 src_url = 'https://lists.xenproject.org/archives/html/mbox/'
@@ -8,6 +9,12 @@ dest_dir = '/home/heather/dev/xen/sample'
 
 repo = MBox(uri=src_url, dirpath=dest_dir)
 logfile = open('mboxAnalysis.log', 'w')
+
+def log_error(error, uuid, is_message):
+    err_log = '\n\nOh potatoes! Something broke: ' + str(error)
+    if is_message:
+        err_log += '\nCheck message with UUID:' + uuid
+    logfile.write(err_log)
 
 def msg_to_bytes(msg):
     data = ''
@@ -18,27 +25,46 @@ def msg_to_bytes(msg):
             pass
     return data.encode('utf-8')
 
-def add_to_ES(doc_type, message, uuid):
-        try:
-            response = es.index(index="test", doc_type=doc_type, id=uuid, body=message)
-        except UnicodeEncodeError as error:
-            logfile.write('\n\nOh potatoes! Something broke: ' + str(error) + '\nCheck message with UUID: ' + uuid + ' to find the offending characters.')
+def get_jwz_message(message, uuid):
+    jwz_msg = Message(msg=message['data'], message_idx=uuid)
+    return jwz_msg
 
-
-prev_msg_hash = None
-prev_uuid = None
-for message in repo.fetch():
+def add_to_ES(doc_type, message):
     try:
-        refs = message['data']['References']
-    except KeyError:
-        pass # First msg in a thread doesn't have references.
-    uuid = message['uuid']
-    msg_in_bytes = msg_to_bytes(message['data'])
-    msg_hash = hashlib.sha256(msg_in_bytes).hexdigest()
-    if not prev_msg_hash == msg_hash and not uuid == prev_uuid:
-        add_to_ES('message', message, uuid)
-        prev_msg_hash = msg_hash
-        prev_uuid = uuid
+        response = es.index(index="test", doc_type=doc_type,
+                id=message['uuid'], body=message)
+    except UnicodeEncodeError as error:
+        log_error(error, message['uuid'], True)        
+
+
+message_map = {}
+def parse_messages(repo):
+    message_list = []
+    prev_msg_hash = None
+    prev_uuid = None
+    for message in repo.fetch():
+        uuid = message['uuid']
+        msg_in_bytes = msg_to_bytes(message['data'])
+        msg_hash = hashlib.sha256(msg_in_bytes).hexdigest()
+        if not prev_msg_hash == msg_hash and not uuid == prev_uuid:
+            message_list.append(get_jwz_message(message, uuid))
+            prev_msg_hash = msg_hash
+            prev_uuid = uuid
+            message_map[uuid] = message
+    return message_list
+
+message_list = parse_messages(repo)
+containers = thread(message_list, group_by_subject=False)
+
+for ctr in containers:
+    try:
+        container = ctr.to_dict()
+        root_message = message_map[container['id']]
+        thread_subject = root_message['data']['Subject']
+        es_type = ''.join(c for c in thread_subject if (c.isalnum() or c.isspace()))
+        add_to_ES(es_type, root_message)
+    except ValueError as error:
+        log_error(error, str(ctr), False)
 
 logfile.close()
 
